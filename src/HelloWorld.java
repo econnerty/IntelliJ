d60fe4da8a160148fd2d51c115e666281df17762
LeotardoDabinci <erikconnerty@gmail.com>
2016-07-05 23:39:45 -0400

    It works finally

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by erikconnerty on 7/5/16.
+ */
+public class HelloWorld
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("IntelliJ finally fucking works");
+  }
+
+
+}
